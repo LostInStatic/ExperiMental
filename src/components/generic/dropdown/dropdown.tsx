0695d824a3a38bfeae7b07cb0016652d8f246b58
LostInStatic <kowal.a1234@gmail.com>
2021-03-16 23:38:54 +0100

    added dropdowns

---
+++ src/components/generic/dropdown/dropdown.tsx
@@ -0,0 +1,35 @@
+import React = require('react');
+import './dropdown.scss';
+//@ts-expect-error
+import {ReactComponent as IconArrow} from '../../../resources/arrow.svg';
+
+interface IProps {
+	buttonLabel: string
+}
+
+const Dropdown: React.FC<IProps> = (props) => {
+	const [displayed, toggleDisplayed] = React.useReducer(toggleState, false);
+
+	return <>
+		<button
+			className={`arrow-button ${displayed ? 'up' : 'down'}`} 
+			onClick={toggleDisplayed}
+		>
+			{props.buttonLabel}
+			<IconArrow/>
+		</button>
+		<div className={
+			`dropdown_content ${displayed ? '' : 'collapsed'
+			}`
+		}>
+			{props.children}
+		</div>
+	</>;
+};
+
+export default Dropdown;
+
+
+const toggleState = (state: boolean) => {
+	return !state;
+};
