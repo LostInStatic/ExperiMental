da7011f99e96b58a71e5b8fe38681fc275dc67e5
LostInStatic <kowal.a1234@gmail.com>
2021-03-17 22:22:29 +0100

    rewritten modals to be more modular

---
+++ src/components/generic/modal/modalBox.tsx
@@ -0,0 +1,32 @@
+import React = require('react');
+import './modal.scss';
+import { createPortal } from 'react-dom';
+
+interface IProps {
+	className?: string
+	displayed: boolean
+	closeButton?: boolean
+}
+
+const ModalBox: React.FC<IProps> = (props) => {
+
+	const modal = <div className={`modal_box ${props.className || ''} ${(props.displayed ? '' : 'collapsed')}`}>
+		<div
+			className='modal_box-content'
+			onClick={e => e.stopPropagation()}
+			
+		>
+			{props.children}
+		</div>
+
+	</div>;
+
+	return <>
+		{
+			createPortal(modal, document.getElementById('root'))
+		}
+
+	</>;
+};
+
+export default ModalBox;
