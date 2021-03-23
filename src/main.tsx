a97f27b75358a947c0020ecd1a2b2f050460a06b
LostInStatic <kowal.a1234@gmail.com>
2021-03-24 00:19:06 +0100

    Cleanup of offline experiment generation not needed anymore

--- src/main.tsx
+++ src/main.tsx
@@ -3,32 +3,7 @@ import ReactDOM = require('react-dom')
 import React = require('react')
 import './style.scss';
 import App from './components/app';
-import getExperiments from './ExperimentsList';
 import fetchRooms from './api/fetchRooms';
-
-const experiments = getExperiments();
-
-//@ts-ignore
-import iconBalloonC from './resources/ingredient-icons/ikona-kolor-balon.svg';
-//@ts-ignore
-import iconBalloonM from './resources/ingredient-icons/ikona-mono-balon.svg';
-//@ts-ignore
-import iconScissC from './resources/ingredient-icons/ikona-kolor-no¾yczki.svg';
-//@ts-ignore
-import iconScissM from './resources/ingredient-icons/ikona-mono-no¾yczki.svg';
-//@ts-ignore
-import iconWaterC from './resources/ingredient-icons/ikona-kolor-woda.svg';
-//@ts-ignore
-import iconWaterM from './resources/ingredient-icons/ikony-mono-woda.svg';
-//@ts-ignore
-import iconSBottM from './resources/ingredient-icons/ikona-mono-butelka.svg';
-//@ts-ignore
-import iconBottC from './resources/ingredient-icons/ikona-kolor-burelka.svg';
-//@ts-ignore
-import iconPapC from './resources/ingredient-icons/ikona-kolor-papier.svg';
-//@ts-ignore
-import iconPapM from './resources/ingredient-icons/ikona-mono-papier.svg';
-
 
 /// #if PRODUCTION
 import registerServiceWorker from './registerServiceWorker';
