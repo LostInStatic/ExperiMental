db4f96c56afd3888a5d4d5ae309ffe458c1b0c68
LostInStatic <kowal.a1234@gmail.com>
2021-03-29 14:36:18 +0200

    added handling for no/invalid category url parameter

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -10,24 +10,30 @@ interface IProps {
 const Category: React.FC<IProps> = (props) => {
 	const urlParams = new URLSearchParams(window.location.search);
 	const data = useData();
-	const [currentIds, setCurrentIds] = React.useState({rooms: [], textBlocks: []});
-	React.useEffect( () => data.request.categories('all'), []);
-	React.useEffect( () => setCurrentIds( selectData(urlParams.get('cat'), data.state.categories.data)), [data.state.categories]); 
+	const [currentIds, setCurrentIds] = React.useState({ rooms: [], textBlocks: [] });
+	React.useEffect(() => data.request.categories('all'), []);
+	React.useEffect(() => setCurrentIds(selectData(urlParams.get('cat'), data.state.categories.data)), [data.state.categories]);
 	return <App
 		roomIds={currentIds.rooms}
 		textBlockIds={currentIds.textBlocks}
-	/> ;
+	/>;
 };
 
 export default Category;
 
 const selectData = (slug: string, categories: ICategoriesData[]) => {
-	if (categories.length === 0) return {rooms: [], textBlocks: []};
-	const currentCategory = categories.filter(category => category.urlSlug === slug)[0];
-	const mainCategory = categories.filter(category => category.urlSlug === APIURLS.mainCategoryQuery)[0];
+	if (categories.length === 0) return { rooms: [], textBlocks: [] };
+	if (!slug) slug = APIURLS.defaultCategorySlug;
+	const currentCategory = categories.filter(
+		category => category.urlSlug === slug
+	)[0] || { roomIds: [], textBlockIds: [] };
+	const mainCategory = categories.filter(
+		category => category.urlSlug === APIURLS.mainCategorySlug
+	)[0] || { roomIds: [], textBlockIds: []};
 
 	return {
 		rooms: [...currentCategory.roomIds, ...mainCategory.roomIds],
 		textBlocks: [...currentCategory.textBlockIds, ...mainCategory.textBlockIds]
 	};
-}; 
+};
+
