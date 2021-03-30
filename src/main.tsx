a176b895682a38c1fec8cbe7c6765981bd2f29f0
LostInStatic <kowal.a1234@gmail.com>
2021-03-30 18:53:29 +0200

    Added categories handling through react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM = require('react-dom')
 import React = require('react')
 import './style.scss';
 import { DataProvider } from './components/dataProvider';
-import Category from './components/category/select';
+import Category from './components/category/current';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 /// #if PRODUCTION
 import registerServiceWorker from './registerServiceWorker';
@@ -14,8 +15,12 @@ console.info('devbuild');
 const urlParams = new URLSearchParams(window.location.search);
 
 ReactDOM.render(
-	<DataProvider>
-		<Category/>
-	</DataProvider>,
+	<Router>
+		<DataProvider>
+			<Switch>
+				<Route path='/:slug'><Category /></Route>
+			</Switch>
+		</DataProvider>
+	</Router>,
 	document.getElementById('root')
 );
