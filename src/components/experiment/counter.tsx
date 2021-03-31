18d99cebd764bc23c6fd1b2ec84f5042924218e2
LostInStatic <kowal.a1234@gmail.com>
2021-03-31 17:19:58 +0200

    added experiments counter

---
+++ src/components/experiment/counter.tsx
@@ -0,0 +1,31 @@
+import React = require('react');
+import { IExperimentsData } from '../../api/fetchExperiments';
+import { useData } from '../dataProvider';
+
+interface IProps {
+	matchedExperiments: IExperimentsData[];
+}
+
+const Counter: React.FC<IProps> = (props) => {
+	const data = useData();
+	const [matched, update] = React.useReducer(addNewlyMatched, {});
+	
+	React.useEffect(
+		() => update(props.matchedExperiments),
+		[props.matchedExperiments]
+	);	
+	
+	return <div className="experiments-counter">
+		{`${Object.keys(matched).length}/${data.state.experiments.data.length}`}
+	</div>;
+};
+
+export default Counter;
+
+const addNewlyMatched = (previous, experiments: IExperimentsData[]) => {
+	let matched = {};
+	experiments.map( (experiment) => {
+		matched[experiment.id] = true;
+	});
+	return {...previous, ...matched};
+};
