bdb4336e7d6f80e0d9072bcee00765b6b0a82071
LostInStatic <kowal.a1234@gmail.com>
2021-04-11 15:33:51 +0200

    Fixed alteration of outgoing links

--- src/api/alterOutgoingLinks.ts
+++ src/api/alterOutgoingLinks.ts
@@ -1,6 +1,6 @@
 const alterOutgoingLinks = (htmlString:string) => {
 	if (!htmlString) return '';
-	return htmlString.replace('<a', '<a target="_blank" ');
+	return htmlString.split('<a').join('<a target="_blank" ');
 };
 
 export default alterOutgoingLinks;
